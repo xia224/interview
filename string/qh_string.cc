7e0801f801fad551518f0ad3519f88b614be5bf4
xia224 <xiazhenya224@163.com>
2014-04-01 15:28:15 +0800

    Update qh_string.cc

--- string/qh_string.cc
+++ string/qh_string.cc
@@ -4,7 +4,7 @@
 
 namespace qh
 {
-    // TODO ½«ÔÚÕâÀïÐ´ÊµÏÖ´úÂë
+    // TODO ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ð´Êµï¿½Ö´ï¿½ï¿½ï¿½
 
     string::string()
         : data_(NULL), len_(0)
@@ -13,25 +13,53 @@ namespace qh
 
     string::string( const char* s )
     {
+        if(s)
+        {
+            len_=strlen(s)+1;
+            data_=new char[len_];
+            strcpy(data_,s);
+        }
+        else
+        {
+            data_=NULL;
+            len_=0;
+        }
     }
 
     string::string( const char* s, size_t len )
     {
+        len_=len+1;
+        data_=new char[len_];
+        strcpy(data_,s);
     }
 
     string::string( const string& rhs )
     {
-
+        len_=rhs.len_;
+        data_=new char[len_];
+        strcpy(data_,rhs.data_);
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(this!=&rhs)
+        {
+            delete[] data_;
+            if(!rhs.data_)
+                data_=NULL;
+            else
+            {
+                data_=new char[strlen(rhs.data_)+1];
+                strcpy(data_,rhs.data_);
+            }
+        }
         return *this;
     }
 
     string::~string()
     {
-
+       delete[] data_;
+       len_=0;
     }
 
     size_t string::size() const
@@ -41,16 +69,17 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
     char* string::operator[]( size_t index )
-    {
-        return NULL;
+    {   
+        assert(index<len_);
+        return data_+index;
     }
 }
